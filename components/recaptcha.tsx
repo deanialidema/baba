1a0d8221d09d4eecd299adbe6e6669dc19136c82
Zk <momqillamerita3@gmail.com>
2025-12-13 08:13:01 -0800

    Update recaptcha.tsx

--- components/recaptcha.tsx
+++ components/recaptcha.tsx
@@ -25,7 +25,7 @@ interface RecaptchaProps {
   theme?: 'light' | 'dark'
 }
 
-const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6Ld42egrAAAAAGn7KlYfvwYT6IhQ2JD8O9ft3SJ4"
+const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6Lf3mCosAAAAAF_TA5XYKr2DtFTxqolBZUUSFSP6"
 
 // Hook to check if reCAPTCHA is loaded
 function useRecaptchaLoaded() {
