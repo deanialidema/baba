0a736dfd2f1fc079d14f072719c2e9e3a380076e
Zk <naturalchoice.al@gmail.com>
2025-12-26 12:44:43 +0100

    Update recaptcha.tsx

--- components/recaptcha.tsx
+++ components/recaptcha.tsx
@@ -25,7 +25,7 @@ interface RecaptchaProps {
   theme?: 'light' | 'dark'
 }
 
-const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6Lc_djcsAAAAAMCocAz4wJShfc_2xlKMZF6ui7Eb"
+
 
 // Hook to check if reCAPTCHA is loaded
 function useRecaptchaLoaded() {
